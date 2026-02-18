7ba77e023144c08d6169d6e0d0e0002df5cdcf42
Chakradhar58 <chakradhargirhepunje43@gmail.com>
2026-02-18 14:03:49 +0530

    first commit

--- db/connectDb.js
+++ db/connectDb.js
@@ -1,16 +1,30 @@
+import mongoose from "mongoose";
 
-import mongoose from "mongoose"; 
-const connectDb = async () => {
-  try {
-    const conn = await mongoose.connect(`mongodb://localhost:27017/chai`);
+const MONGODB_URI = process.env.MONGODB_URI;
 
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
-    return conn;
-  } catch (error) {
-    console.error(error.message);
-    process.exit(1);
-  }
+if (!MONGODB_URI) {
+  throw new Error("Please define MONGODB_URI in environment variables");
 }
 
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
+const connectDb = async () => {
+  if (cached.conn) {
+    return cached.conn;
+  }
+
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(MONGODB_URI).then((mongoose) => {
+      return mongoose;
+    });
+  }
+
+  cached.conn = await cached.promise;
+  return cached.conn;
+};
 
 export default connectDb;
